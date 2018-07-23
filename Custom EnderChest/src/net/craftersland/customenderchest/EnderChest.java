d95b08c6eefff6a33576cf618e7b625b1cae98c6
montlikadani <montlikada@gmail.com>
2018-07-23 20:50:51 +0200

    Update EnderChest.java

--- Custom EnderChest/src/net/craftersland/customenderchest/EnderChest.java
+++ Custom EnderChest/src/net/craftersland/customenderchest/EnderChest.java
@@ -21,12 +21,12 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EnderChest extends JavaPlugin {
-	
+
 	public static Logger log;
 	public Map<Inventory, UUID> admin = new HashMap<Inventory, UUID>();
 	public static boolean is19Server = true;
 	public static String pluginName = "CustomEnderChest";
-	
+
 	private static ConfigHandler configHandler;
 	private static StorageInterface storageInterface;
 	private static EnderChestUtils enderchestUtils;
@@ -35,98 +35,106 @@ public class EnderChest extends JavaPlugin {
 	private static SoundHandler sH;
 	private static ModdedSerializer ms;
 	private static FileToMysqlCmd ftmc;
-	
-		public void onEnable() {
-			log = getLogger();
-			getMcVersion();	    	
-	        configHandler = new ConfigHandler(this);
-	        checkForModdedNBTsupport();
-	        enderchestUtils = new EnderChestUtils(this);
-	        dH = new DataHandler();
-	        if (configHandler.getString("database.typeOfDatabase").equalsIgnoreCase("mysql") == true) {
-	        	log.info("Using MySQL database for data.");
-	        	mysqlSetup = new MysqlSetup(this);
-	        	storageInterface = new MysqlStorage(this);
-	        } else {
-	        	log.info("Using FlatFile system for data.");
-	        	File pluginFolder = new File("plugins" + System.getProperty("file.separator") + pluginName + System.getProperty("file.separator") + "PlayerData");
-	    		if (pluginFolder.exists() == false) {
-	        		pluginFolder.mkdir();
-	        	}
-		      	storageInterface = new FlatFileStorage(this);	
+
+	@Override
+	public void onEnable() {
+		log = getLogger();
+		getMcVersion();	    	
+		configHandler = new ConfigHandler(this);
+		checkForModdedNBTsupport();
+		enderchestUtils = new EnderChestUtils(this);
+		dH = new DataHandler();
+		if (configHandler.getString("database.typeOfDatabase").equalsIgnoreCase("mysql") == true) {
+	        log.info("Using MySQL database for data.");
+	        mysqlSetup = new MysqlSetup(this);
+	        storageInterface = new MysqlStorage(this);
+		} else {
+	        log.info("Using FlatFile system for data.");
+	        File pluginFolder = new File("plugins" + System.getProperty("file.separator") + pluginName + System.getProperty("file.separator") + "PlayerData");
+	    	if (!pluginFolder.exists()) {
+	        	pluginFolder.mkdir();
 	        }
-	        sH = new SoundHandler(this);
-	        ftmc = new FileToMysqlCmd(this);
-	    	PluginManager pm = getServer().getPluginManager();
-	    	pm.registerEvents(new PlayerHandler(this), this);
-	    	CommandHandler cH = new CommandHandler(this);
-	    	getCommand("customec").setExecutor(cH);
-	    	getCommand("ec").setExecutor(cH);
-	    	getCommand("customenderchest").setExecutor(cH);
-	    	log.info(pluginName + " loaded successfully!");
+			storageInterface = new FlatFileStorage(this);	
 		}
-		
-		//Disabling plugin
-		public void onDisable() {
-			HandlerList.unregisterAll(this);
-			Bukkit.getScheduler().cancelTasks(this);
+		sH = new SoundHandler(this);
+		ftmc = new FileToMysqlCmd(this);
+	    PluginManager pm = getServer().getPluginManager();
+	    pm.registerEvents(new PlayerHandler(this), this);
+	    CommandHandler cH = new CommandHandler(this);
+	    getCommand("customec").setExecutor(cH);
+	    getCommand("ec").setExecutor(cH);
+	    getCommand("customenderchest").setExecutor(cH);
+	    log.info(pluginName + " loaded successfully!");
+	}
+
+	@Override
+	public void onDisable() {
+		HandlerList.unregisterAll(this);
+		Bukkit.getScheduler().cancelTasks(this);
+		if (configHandler.getString("database.typeOfDatabase").equalsIgnoreCase("mysql")) {
+			if (mysqlSetup.getConnection() != null) {
+				mysqlSetup.closeDatabase();
+			}
+		}
+		log.info(pluginName + " is disabled!");
+	}
+
+	private boolean getMcVersion() {
+		String[] serverVersion = Bukkit.getBukkitVersion().split("-");
+		String version = serverVersion[0];
+
+		if (version.matches("1.7.10") || version.matches("1.7.9") || version.matches("1.7.5") || version.matches("1.7.2") || version.matches("1.8.8") || version.matches("1.8.3") || version.matches("1.8.4") || version.matches("1.8")) {
+		    is19Server = false;
+		    return true;
+		}
+		return false;
+	}
+
+	private void checkForModdedNBTsupport() {
+		if (configHandler.getBoolean("settings.modded-NBT-data-support") == true) {
 			if (configHandler.getString("database.typeOfDatabase").equalsIgnoreCase("mysql")) {
-				if (mysqlSetup.getConnection() != null) {
-					mysqlSetup.closeDatabase();
-				}
-			}
-			log.info(pluginName + " is disabled!");
-		}
-		
-		private boolean getMcVersion() {
-			String[] serverVersion = Bukkit.getBukkitVersion().split("-");
-		    String version = serverVersion[0];
-		    
-		    if (version.matches("1.7.10") || version.matches("1.7.9") || version.matches("1.7.5") || version.matches("1.7.2") || version.matches("1.8.8") || version.matches("1.8.3") || version.matches("1.8.4") || version.matches("1.8")) {
-		    	is19Server = false;
-		    	return true;
-		    }
-		    return false;
-		}
-		
-		private void checkForModdedNBTsupport() {
-			if (configHandler.getBoolean("settings.modded-NBT-data-support") == true) {
-				if (configHandler.getString("database.typeOfDatabase").equalsIgnoreCase("mysql")) {
-					if (Bukkit.getPluginManager().getPlugin("ProtocolLib") != null) {
-						ms = new ModdedSerializer(this);
-						log.info("ProtocolLib dependency found. Modded NBT data support is enabled!");
-			        } else {
-			        	log.warning("ProtocolLib dependency not found!!! Modded NBT data support is disabled!");
-			        }
+				if (Bukkit.getPluginManager().getPlugin("ProtocolLib") != null) {
+					ms = new ModdedSerializer(this);
+					log.info("ProtocolLib dependency found. Modded NBT data support is enabled!");
 				} else {
-					log.warning("NBT Modded data support only works for MySQL storage. Modded NBT data support is disabled!");
+			        log.warning("ProtocolLib dependency not found!!! Modded NBT data support is disabled!");
 				}
+			} else {
+				log.warning("NBT Modded data support only works for MySQL storage. Modded NBT data support is disabled!");
 			}
-	    }
-		
-		public ConfigHandler getConfigHandler() {
-			return configHandler;
 		}
-		public StorageInterface getStorageInterface() {
-			return storageInterface;
-		}
-		public EnderChestUtils getEnderChestUtils() {
-			return enderchestUtils;
-		}
-		public MysqlSetup getMysqlSetup() {
-			return mysqlSetup;
-		}
-		public SoundHandler getSoundHandler() {
-			return sH;
-		}
-		public DataHandler getDataHandler() {
-			return dH;
-		}
-		public ModdedSerializer getModdedSerializer() {
-			return ms;
-		}
-		public FileToMysqlCmd getFileToMysqlCmd() {
-			return ftmc;
-		}
+	}
+
+	public ConfigHandler getConfigHandler() {
+		return configHandler;
+	}
+
+	public StorageInterface getStorageInterface() {
+		return storageInterface;
+	}
+
+	public EnderChestUtils getEnderChestUtils() {
+		return enderchestUtils;
+	}
+
+	public MysqlSetup getMysqlSetup() {
+		return mysqlSetup;
+	}
+
+	public SoundHandler getSoundHandler() {
+		return sH;
+	}
+
+	public DataHandler getDataHandler() {
+		return dH;
+	}
+
+	public ModdedSerializer getModdedSerializer() {
+		return ms;
+	}
+
+	public FileToMysqlCmd getFileToMysqlCmd() {
+		return ftmc;
+	}
 
 }
