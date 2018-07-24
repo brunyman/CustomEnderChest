60abf6e4945c58c00c4394917a4c6327f3a785fc
montlikadani <montlikada@gmail.com>
2018-07-24 14:18:03 +0200

    Update DataHandler.java

--- Custom EnderChest/src/net/craftersland/customenderchest/DataHandler.java
+++ Custom EnderChest/src/net/craftersland/customenderchest/DataHandler.java
@@ -7,25 +7,24 @@ import java.util.UUID;
 import org.bukkit.inventory.Inventory;
 
 public class DataHandler {
-	
+
 	private Map<UUID, Inventory> liveData = new HashMap<UUID, Inventory>();
-	
+
 	public DataHandler() {
-		
 	}
-	
+
 	public Inventory getData(UUID playerUUID) {
 		return liveData.get(playerUUID);
 	}
-	
+
 	public void setData(UUID playerUUID, Inventory enderchestInventory) {
 		liveData.put(playerUUID, enderchestInventory);
 	}
-	
+
 	public void removeData(UUID playerUUID) {
 		liveData.remove(playerUUID);
 	}
-	
+
 	public boolean isLiveEnderchest(Inventory inventory) {
 		return liveData.containsValue(inventory);
 	}
