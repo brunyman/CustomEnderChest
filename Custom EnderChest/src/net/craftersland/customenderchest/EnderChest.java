95204c4597c9642d1982ee540d9c543063cefeae
brunyman <brunyman@BrunyMan-PC>
2019-07-24 16:55:13 +0300

    1.14.4 API Update

--- Custom EnderChest/src/net/craftersland/customenderchest/EnderChest.java
+++ Custom EnderChest/src/net/craftersland/customenderchest/EnderChest.java
@@ -88,15 +88,23 @@ public class EnderChest extends JavaPlugin {
 		    
 		    if (version.matches("1.7.10") || version.matches("1.7.9") || version.matches("1.7.5") || version.matches("1.7.2") || version.matches("1.8.8") || version.matches("1.8.3") || version.matches("1.8.4") || version.matches("1.8")) {
 		    	is19Server = false;
+		    	log.info("Compatible server version detected: " + version);
 		    	return true;
 		    } else if (version.matches("1.13") || version.matches("1.13.1") || version.matches("1.13.2")) {
 		    	is19Server = true;
 		    	is13Server = true;
+		    	log.info("Compatible server version detected: " + version);
 		    	return true;
-		    } else if (version.matches("1.14") || version.matches("1.14.1") || version.matches("1.14.2") || version.matches("1.14.3")) {
+		    } else if (version.matches("1.14") || version.matches("1.14.1") || version.matches("1.14.2") || version.matches("1.14.3") || version.matches("1.14.4")) {
 		    	is19Server = true;
 		    	is13Server = true;
+		    	log.info("Compatible server version detected: " + version);
 		    	return true;
+		    } else {
+		    	//Default fallback to 1.14 API
+		    	is19Server = true;
+		    	is13Server = true;
+		    	log.info("Incompatible server version detected: " + version + " . Running into 1.14 API mode.");
 		    }
 		    return false;
 		}
